0729f5ea422322c5bd0ad311292f486424def511
AlexReid95 <alexreid95@gmail.com>
2020-05-07 19:38:41 +0100

    Refactor and Add play again button Relates #8

--- src/getPokemonData.js
+++ src/getPokemonData.js
@@ -1,14 +1,15 @@
 const checkResponse = (response) => {
   if (response.status !== 200) {
     console.log(`Error has occurred, status is ${response.status}`);
-    throw new Error(`Pokemon couldn't be caught!`)
+    throw new Error(`Pokemon couldn't be caught!`);
   }
   return response.json();
 };
 
 const getPokemonData = (input) => {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
-    .then(checkResponse)
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${input}`).then(
+    checkResponse
+  );
 };
 
 export default getPokemonData;
